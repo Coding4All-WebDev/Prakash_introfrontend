9345bf83908cd80c1c1ab9b41200483d6029d94c
prajapatiprakash4216 <112812902+prajapatiprakash4216@users.noreply.github.com>
2022-11-22 18:21:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//1..
+
+
+//  function student (stu_id,stu_div)
+//  {
+//      this.stu_id=stu_id;
+//        this.stu_div;
+//  }
+
+//   var rohan = new student(5242,'A');
+//   console.log(rohan)
+
+
+//2..
+// function Person(first, last, age, eyecolor) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eyecolor;
+//   }
+  
+//   const myFather = new Person("rohan", "pal", 50, "blue");
+//   const myMother = new Person("anjali", "pal", 48, "green");
+//   console.log(myFather)
+//   console.log(myMother)
